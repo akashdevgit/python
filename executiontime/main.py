5f794a29c7eb4757937f4c031ad234d0963cf0e9
akashdevgit <akashkn23@gmail.com>
2024-06-28 23:51:54 +0530

    executiomtimeprogram

---
+++ executiontime/main.py
@@ -0,0 +1,17 @@
+#This program aims to calculate execution time of a python script in seconds 
+#Idea: Store initiation time in a variable called 'timestart' and completion time in a variable called 'timeend' and get the difference between them to compute exection time 
+# - Akash 
+
+import time 
+
+timestart = time.time() 
+
+word = "Akash" 
+word += "CSE"
+print(word)
+
+
+
+timeend = time.time() 
+
+print(f"\The execution time of the program is {timeend-timestart}")
